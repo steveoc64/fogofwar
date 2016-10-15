f1c4dbd6ecb1c5b9efc662033e5e7ff672994ab3
SteveOC64 <steveoc64@gmail.com>
2016-10-15 13:48:39 +1030

    added backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+date=`date "+%Y%m%d-%H%M%S"`
+file="actionfront-$date"
+echo $file
+pg_dump actionfront | gzip > ../backup/$file.sqz
+cd ../backup
+echo Latest Backup File
+ls -l $file.sqz
+echo
+echo All Backups
+ls -s
+echo 
+echo Files older than 14 days
+find . -mtime +14
+

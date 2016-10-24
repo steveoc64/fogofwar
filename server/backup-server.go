79f45a701bfce3173d9374715a84796f9c4447b0
SteveOC64 <steveoc64@gmail.com>
2016-10-25 08:03:25 +1030

    autobackup schedules properly

--- server/backup-server.go
+++ server/backup-server.go
@@ -12,15 +12,17 @@ func autoBackup() {
 	go func() {
 
 		// Init Hours to be the hour of the day
-		hours := time.Now().Hour()
+		// hours := time.Now().Hour()
+		hours := 0
 
 		for {
 			time.Sleep(1 * time.Hour)
 
 			hours++
 			if hours >= 24 {
-				hours = time.Now().Hour()
-				log.Println("24 Hours - db backup")
+				hours = 0
+				// hours = time.Now().Hour()
+				log.Println("Running for 24 Hours - auto db backup")
 				out, err := exec.Command("../scripts/backup.sh").Output()
 				if err != nil {
 					log.Println(err)

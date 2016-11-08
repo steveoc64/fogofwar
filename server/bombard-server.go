ee8b63523f4df96b13210884ba26e4bd38a86acc
SteveOC64 <steveoc64@gmail.com>
2016-11-08 15:51:16 +1030

    add bombard calc and all gunnery tables

--- server/bombard-server.go
+++ server/bombard-server.go
@@ -137,11 +137,6 @@ func (g *GameRPC) ComputeBombard(data shared.BombardData, done *bool) error {
 	if err != nil {
 		println(err.Error())
 	} else {
-		DB.SQL(`select * from unit where path <@ $1 and game_id=$2 and cmd_id=$3`,
-			target.Path,
-			target.GameID,
-			target.CmdID).QueryStructs(&subUnits)
-		println("Main target formation is ", target.Path)
 		err = DB.SQL(`select * from bombard where id=$1`, data.ID).QueryStruct(bb)
 		// fmt.Printf("bb %d %v\n", data.ID, *bb)
 		if err != nil {
@@ -157,18 +152,44 @@ func (g *GameRPC) ComputeBombard(data shared.BombardData, done *bool) error {
 			err = DB.SQL(`select guns,gunnery_type from unit where id=$1`, bb.UnitID).QueryScalar(&guns, &gtype)
 
 			// Take 1 shot at the min range, and 2 shots at the max range
-			pts = gunShotGood(guns, gtype, bb.RangeMin)
-			pts += gunShotGood(guns, gtype, bb.RangeMax)
-			pts += gunShotGood(guns, gtype, bb.RangeMax)
-			println("total damage points", pts)
+			pts = gunShot(guns, gtype, bb.RangeMin, true)
+			pts += gunShot(guns, gtype, bb.RangeMax, true)
+			pts += gunShot(guns, gtype, bb.RangeMax, true)
+			// println("total damage points", pts)
+
+			DB.SQL(`select * from unit where path <@ $1 and game_id=$2 and cmd_id=$3`,
+				target.Path,
+				target.GameID,
+				target.CmdID).QueryStructs(&subUnits)
+
+			// compute weighted chances of hitting
+			prospects := []int{}
+			for ii, v := range subUnits {
+				i := 1
+				if v.UType != shared.UnitDiv {
+					switch v.Role {
+					case shared.RoleReserve:
+						i = 1
+					case shared.RoleAdvance:
+						i = 5
+					case shared.Role1:
+						i = 3
+					case shared.Role2, shared.RoleLeft, shared.RoleRight:
+						i = 2
+					}
+				}
+				for j := 0; j < i; j++ {
+					prospects = append(prospects, ii)
+				}
+			}
+			victim := subUnits[rand.Intn(len(prospects))]
 
 			// pick a random victim
-			victim := subUnits[rand.Intn(len(subUnits))]
-			println("Actual Victim that takes the hit:", victim.Path, victim.ID, victim.UType, victim.Name, victim.Path)
-			println("Regular troops in 3 ranks damage =", victim.PtsToMen(pts, 3, false, false))
-			println("If in column damage =", victim.PtsToMen(pts, 3, true, false))
-			println("If in skirmish order damage =", victim.PtsToMen(pts, 3, false, true))
-			println("If in 2 ranks damage =", victim.PtsToMen(pts, 2, false, false))
+			// println("Actual Victim that takes the hit:", victim.Path, victim.ID, victim.UType, victim.Name, victim.Path)
+			// println("Regular troops in 3 ranks damage =", victim.PtsToMen(pts, 3, false, false))
+			// println("If in column damage =", victim.PtsToMen(pts, 3, true, false))
+			// println("If in skirmish order damage =", victim.PtsToMen(pts, 3, false, true))
+			// println("If in 2 ranks damage =", victim.PtsToMen(pts, 2, false, false))
 		}
 	}
 
@@ -189,7 +210,7 @@ type GunTables struct {
 	Effect      [][]int
 }
 
-func gunShotGood(guns int, gtype int, r int) int {
+func gunShot(guns int, gtype int, r int, good bool) int {
 	if guns < 1 {
 		return 0
 	}
@@ -202,6 +223,29 @@ func gunShotGood(guns int, gtype int, r int) int {
 	mult := float64(guns) / 8.0
 	die := rand.Intn(6)
 	table := shotGoodEffect[gtype-1]
+	if !good {
+		table = shotBadEffect[gtype-1]
+	}
+	effects := table.Effect[r-1]
+	return int(float64(effects[die]) * mult)
+}
+
+func cannisterShot(guns int, gtype int, r int, good bool) int {
+	if guns < 1 {
+		return 0
+	}
+	if gtype < 1 || gtype > 5 {
+		return 0
+	}
+	if r < 1 || r > 3 {
+		return 0
+	}
+	mult := float64(guns) / 8.0
+	die := rand.Intn(6)
+	table := cannisterGoodEffect[gtype-1]
+	if !good {
+		table = cannisterBadEffect[gtype-1]
+	}
 	effects := table.Effect[r-1]
 	return int(float64(effects[die]) * mult)
 }
@@ -237,4 +281,83 @@ var shotGoodEffect = []GunTables{
 		{6, 4, 4, 5, 2, 5},
 		{3, 2, 2, 2, 1, 2},
 		{0, 0, 0, 0, 0, 0}}},
+}
+
+var shotBadEffect = []GunTables{
+	{1, "12lb", 5, [][]int{
+		{7, 7, 4, 7, 7, 4},
+		{5, 5, 3, 5, 5, 3},
+		{2, 2, 2, 2, 2, 2},
+		{2, 2, 2, 2, 2, 2},
+		{1, 1, 1, 1, 1, 1}}},
+	{2, "9lb", 5, [][]int{
+		{6, 6, 8, 5, 3, 3},
+		{3, 4, 5, 3, 2, 2},
+		{1, 2, 2, 1, 1, 1},
+		{2, 2, 3, 2, 1, 1},
+		{0, 0, 0, 0, 0, 0}}},
+	{3, "6lb", 4, [][]int{
+		{5, 6, 8, 5, 3, 3},
+		{3, 4, 5, 3, 2, 2},
+		{1, 2, 2, 1, 1, 1},
+		{2, 2, 3, 2, 1, 1},
+		{0, 0, 0, 0, 0, 0}}},
+	{4, "Light", 3, [][]int{
+		{1, 2, 2, 1, 1, 1},
+		{2, 2, 3, 2, 1, 1},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0}}},
+	{5, "Howitzer", 4, [][]int{
+		{2, 3, 2, 3, 5, 3},
+		{2, 3, 2, 3, 5, 3},
+		{1, 2, 1, 2, 4, 2},
+		{0, 1, 0, 1, 2, 1},
+		{0, 0, 0, 0, 0, 0}}},
+}
+
+var cannisterGoodEffect = []GunTables{
+	{1, "12lb", 5, [][]int{
+		{40, 30, 15, 26, 26, 15},
+		{27, 20, 10, 16, 17, 10},
+		{21, 16, 8, 14, 14, 8}}},
+	{2, "9lb", 5, [][]int{
+		{24, 20, 12, 32, 24, 20},
+		{18, 13, 8, 21, 16, 13},
+		{12, 10, 6, 16, 12, 10}}},
+	{3, "6lb", 4, [][]int{
+		{24, 20, 12, 32, 24, 20},
+		{16, 13, 8, 21, 16, 13},
+		{12, 10, 6, 16, 12, 10}}},
+	{4, "Light", 3, [][]int{
+		{16, 8, 6, 16, 19, 6},
+		{10, 9, 4, 12, 6, 4},
+		{0, 0, 0, 0, 0, 0}}},
+	{5, "Howitzer", 4, [][]int{
+		{11, 7, 7, 8, 5, 8},
+		{11, 7, 7, 8, 5, 8},
+		{8, 5, 5, 6, 3, 6}}},
+}
+
+var cannisterBadEffect = []GunTables{
+	{1, "12lb", 5, [][]int{
+		{9, 9, 7, 9, 9, 7},
+		{6, 6, 5, 6, 5, 5},
+		{4, 4, 4, 4, 4, 4}}},
+	{2, "9lb", 5, [][]int{
+		{5, 12, 8, 5, 4, 4},
+		{3, 8, 5, 3, 3, 3},
+		{3, 6, 4, 3, 2, 2}}},
+	{3, "6lb", 4, [][]int{
+		{5, 12, 8, 5, 4, 4},
+		{3, 8, 5, 3, 3, 3},
+		{3, 6, 4, 3, 2, 2}}},
+	{4, "Light", 3, [][]int{
+		{4, 8, 5, 3, 3, 1},
+		{2, 4, 1, 1, 1, 2},
+		{0, 0, 0, 0, 0, 0}}},
+	{5, "Howitzer", 4, [][]int{
+		{3, 4, 3, 5, 7, 4},
+		{3, 4, 3, 5, 7, 4},
+		{2, 3, 2, 3, 5, 3}}},
 }
